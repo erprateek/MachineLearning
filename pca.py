edb99bd3dfd41ea13418676f36d919952d8a6877
Prateek Tandon <ptandon1@andrew.cmu.edu>
2025-04-24 07:14:26 -0700

    Update pca.py

--- pca.py
+++ pca.py
@@ -2,11 +2,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
